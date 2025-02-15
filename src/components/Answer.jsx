9daf510c1b4de7b26369c193cfd8b1016cd039ed
Kai Liang <kailiang.toh@screeningeagle.com>
2025-02-15 14:00:56 +0800

    Architecture

---
+++ src/components/Answer.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Answer;
---
+++ src/components/AnswersGroup.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AnswersGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default AnswersGroup;
