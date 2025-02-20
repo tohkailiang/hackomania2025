9178f4e328e92cb3dc5fe7a6112e700379985a43
tohkailiang <tohkailiang@gmail.com>
2025-02-20 12:20:49 +0800

    Fixed pathing in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "hackomania2025", // Change to your repo name
+  base: "/hackomania2025/", // Change to your repo name
   plugins: [react()],
 });
 
