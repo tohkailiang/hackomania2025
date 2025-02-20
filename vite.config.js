b67f7e7bcc78f6255689403c9e1a3e89bd52d7b6
tohkailiang <tohkailiang@gmail.com>
2025-02-20 12:02:59 +0800

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "hackomania2025", // Change to your repo name
   plugins: [react()],
-})
+});
+
